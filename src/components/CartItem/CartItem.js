6eb120334d8fa06273796d28403b2032763bb514
Nguyen Thai Ngoc <ntngoc96>
2019-08-31 11:52:12 +0700

    improvement/cart: discount,totalprice fix bug, fix some style

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import {
   Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, Button, Col, Row
@@ -16,28 +17,44 @@ const propTypes = {
 function format_curency(a) {
   return String(a).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
 }
-const getPriceAfterDiscount = (oldPrice,discount = 10) => {
+const getPriceAfterDiscount = (oldPrice, discount) => {
   oldPrice = parseInt(oldPrice);
-  return oldPrice - oldPrice*(discount/100);
+  discount = parseInt(discount);
+  return oldPrice - oldPrice * (discount / 100);
 }
 
 export class CartItem extends Component {
-  
+
   render() {
     return (
-      <div className="cart-item">
+      <div className="cart-item u-margin-bottom-small">
         <Card className="cart-item__img">
           <CardImg top src={Image} alt="Card image cap" />
         </Card>
         <Card className="cart-item__content">
           <CardBody>
             <CardTitle>{this.props.name}</CardTitle>
-            <CardSubtitle className="u-text-decoration-linethough text-muted u-font-size-small">{format_curency(this.props.price)}</CardSubtitle>
-            <CardSubtitle>{(format_curency(getPriceAfterDiscount(this.props.price),this.props.discount))} VNĐ</CardSubtitle>
-            
+            <CardSubtitle className={classNames("u-text-decoration-linethough text-muted u-font-size-small",{ invisible: this.props.discount === 0})}>{format_curency(this.props.price)}</CardSubtitle>
+            <CardSubtitle>{(format_curency(getPriceAfterDiscount(this.props.price, this.props.discount)))} VNĐ</CardSubtitle>
+            <AppContext.Consumer>
+              {({ increaseAmount, decreaseAmount }) => (
+                <div className="cart-item__group">
+                  <div>Số lượng:</div>
+                  <button className="cart-item__group--button cart-item__group--button-left"
+                    onClick={() => decreaseAmount(this.props._id)}>
+                    -
+                  </button>
+                  <span className="cart-item__group--span">{this.props.amount}</span>
+                  <button className="cart-item__group--button cart-item__group--button-right"
+                    onClick={() => increaseAmount(this.props._id)}>
+                    +
+                  </button>
+                </div>
+              )}
+            </AppContext.Consumer>
             <AppContext.Consumer>
               {({ deleteFormCart }) => (
-                <Button className="u-margin-top-small cart-item__button u-font-size-small btn-danger" onClick={() => deleteFormCart(this.props)}>Xóa</Button>
+                <Button className="u-margin-top-small cart-item--button u-font-size-small btn-danger" onClick={() => deleteFormCart(this.props)}>Xóa</Button>
 
               )}
             </AppContext.Consumer>
--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { AppContext } from '../../contexts';
 import { CartItem } from '../../components/CartItem';
 import { Link } from 'react-router-dom';
+import { Table } from 'reactstrap';
+import { format } from 'path';
 
 
 const propTypes = {
@@ -13,36 +15,54 @@ function format_curency(a) {
   return String(a).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
 }
 export class Cart extends Component {
-  constructor(props) {
-    super(props);
 
-    // this.state = {
-    //   totalPrice: 0
-    // }
-
-  }
-
-  checkEmptyCart = (CartItems) => {
+  checkEmptyAndRenderCart = (CartItems, AmountItems,Discount) => {
     if (CartItems.length === 0) {
       return <div>Hiện không có sản phẩm nào trong giỏ hàng, tiến hành
         <Link to="/"> đặt dịch vụ ngay!!</Link>
       </div>
     } else {
       return CartItems.map((value, index) => {
-        return <CartItem {...value} discount="10" key={index} />;
+        return <CartItem {...value} amount={AmountItems[index]} key={index} />;
       }
       )
     }
+  }
+
+  renderTotalBill = (CartItems,AmountItems,Discount) => {
+    Discount = parseInt(Discount);
+    
+    return CartItems.map((value,index) => <tr>
+        <td>{value.name}</td>
+        <td>{AmountItems[index]}</td>
+        <td>{ format_curency(parseInt(value.price)*AmountItems[index] - parseInt(value.price)*AmountItems[index]*(value.discount/100))}</td>
+      </tr>
+    )
   }
 
   render() {
     return (
       <div>
         <AppContext.Consumer>{
-          ({ CartItems,TotalPrice }) => (
+          ({ CartItems, TotalPrice, AmountItems,Discount }) => (
             <div className="cart__container">
-              <div className="cart__left"> {this.checkEmptyCart(CartItems)}</div>
-              <div className="cart__right">{TotalPrice}</div>
+              <div className="cart__left"> {this.checkEmptyAndRenderCart(CartItems, AmountItems,Discount)}</div>
+              <div className="cart__right">
+                <div className="text-left u-margin-bottom-small">Chi tiết giá</div>
+                <Table size="sm" hover responsive>
+                  <thead>
+                    <tr>
+                      <th>Tên dịch vụ</th>
+                      <th>Số lượng</th>
+                      <th>Giá tiền</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {this.renderTotalBill(CartItems,AmountItems,Discount)}
+                  </tbody>
+                </Table>
+                <div className="text-right u-margin-top-medium">Tổng cộng: {format_curency(TotalPrice)}</div>
+              </div>
             </div>
           )}
         </AppContext.Consumer>
