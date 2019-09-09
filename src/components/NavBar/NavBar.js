a37f80985636802b984df597d621ddfded1437f2
Nguyen Thai Ngoc <ntngoc96>
2019-09-09 20:51:02 +0700

    improvement/homepage improve style css

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,56 +1,65 @@
 import React from 'react';
-import { Container, Row, Col, Nav, NavItem } from 'reactstrap';
+import {  Row, Col, Nav, NavItem } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
-import logo from '../../image/yellow-cat-logo.png';
+import logo from '../../image/logo_transparent.png';
 
 export class NavBar extends React.Component {
   render() {
     return (
-      <Container>
+      <div className="navigation">
         <Row className="py-3 ">
           <Col>
             <Nav>
               <NavItem>
-                <img src={logo} width="100" alt="logo" />
+                <img src={logo} width="50%" className="nav__logo" alt="logo" />
               </NavItem>
             </Nav>
           </Col>
-          <Col xs="auto" className="font-weight-bold">
+          <Col xs="auto" className="navigation_nav font-weight-bold">
             <Nav>
-              <NavItem>
+              <NavItem className="nav__item">
                 
-                  <NavLink to="/">Trang chủ</NavLink>
-                
-              </NavItem>
-              <NavItem>
-                
-                  <NavLink to="/introduction">Giới thiệu</NavLink>
+                  <NavLink className="nav__link" to="/">Trang chủ</NavLink>
                 
               </NavItem>
-              <NavItem>
+              <NavItem className="nav__item">
                 
-                  <NavLink to="/product">Sản phẩm</NavLink>
-                
-              </NavItem>
-              <NavItem>
-                
-                  <NavLink to="/news">Tin tức</NavLink>
+                  <NavLink className="nav__link" to="/introduction">Giới thiệu</NavLink>
                 
               </NavItem>
-              <NavItem>
+              <NavItem className="nav__item">
                 
-                  <NavLink to="/promotion">Khuyến mãi</NavLink>
+                  <div className=" nav__service" >
+                    <div className="nav__link">Dịch vụ &nbsp;<i className="fa fa-chevron-down"></i></div>
+                    <div className="service__group">
+                      <div className="service__item">Nâng cơ trẻ hóa da</div>
+                      <div className="service__item">Tiêm filler căn mọng</div>
+                      <div className="service__item">Botox thon gọn hàm</div>
+                      <div className="service__item">Truyền trắng tinh khôi</div>
+                      <div className="service__item">Tiêm tan mỡ đa liệu trình</div>
+                    </div>
+                  </div>
                 
               </NavItem>
-              <NavItem>
+              <NavItem className="nav__item">
                 
-                  <NavLink to="/add-product">Liên hệ</NavLink>
+                  <NavLink className="nav__link" to="/news">Tin tức &nbsp;<i className="fa fa-chevron-down"></i></NavLink>
+                
+              </NavItem>
+              <NavItem className="nav__item">
+                
+                  <NavLink className="nav__link" to="/promotion">Khuyến mãi</NavLink>
+                
+              </NavItem>
+              <NavItem className="nav__item">
+                
+                  <NavLink className="nav__link" to="/add-product">Liên hệ</NavLink>
                 
               </NavItem>
             </Nav>
           </Col>
         </Row>
-      </Container>
+      </div>
     );
   }
 }
--- src/components/New/New.js
+++ src/components/New/New.js
@@ -7,23 +7,81 @@ import { Item } from '../Item/index';
 
 import { responsive } from '../../config';
 
+const
+  speed = 1200,
+  curveDelay = 300,
+  position = "fixed"; // or absolute
+
 export class New extends React.Component {
   constructor() {
     super();
 
     this.processItemsToChildren = this.processItemsToChildren.bind(this);
+    this.addItem = this.addItem.bind(this);
   }
 
-  processItemsToChildren(Items = []) {
+  flyToCart() {
+
+  }
+
+  addItem(e, cart, image) {
+    let btnY =
+      position === "fixed"
+        ? e.currentTarget.getBoundingClientRect().top
+        : e.currentTarget.offsetTop,
+      btnX =
+        position === "fixed"
+          ? e.currentTarget.getBoundingClientRect().left
+          : e.currentTarget.offsetLeft;
+
+    let flyingBtn = document.createElement('img');
+    flyingBtn.src = image;
+
+
+
+
+    flyingBtn.classList.add("flyingBtn");
+    flyingBtn.style.position = position;
+    flyingBtn.style.top = `${btnY - 100}px`;
+    flyingBtn.style.left = `${btnX}px`;
+
+    console.log(btnX, " ---- ", btnY);
+
+    flyingBtn.style.opacity = "1";
+    flyingBtn.style.height = "10rem";
+    flyingBtn.style.width = "10rem";
+    flyingBtn.style.transition = `all ${speed / 1000}s ease, top ${(speed +
+      curveDelay) /
+      1000}s ease, left ${speed / 1000}s ease, transform ${speed /
+      1000}s ease ${(speed - 10) / 1000}s`;
+
+    document.body.appendChild(flyingBtn);
+
+
+    flyingBtn.style.top = `${cart.getBoundingClientRect().top + (cart.offsetHeight / 2)}px`;
+    flyingBtn.style.left = `${cart.getBoundingClientRect().left + (cart.offsetWidth / 2)}px`;
+    flyingBtn.style.height = "1rem";
+    flyingBtn.style.width = "1rem";
+    flyingBtn.style.transform = "scale(0)";
+
+    setTimeout(() => {
+      flyingBtn.remove();
+    }, speed * 1.4);
+  }
+
+  processItemsToChildren(Items = [], cart) {
     return Items.map((value, index) =>
-      <Item {...value} key={index} />
+      <Item {...value} key={index} addItem={this.addItem} cart={cart} />
     )
   }
-  
+
 
   render() {
 
-    const { Items } = this.props;
+    const { Items, cart } = this.props;
+
+    console.log(this.props.cart);
+
 
     if (Items.length === 0) {
       return <div> Loading...</div>
@@ -49,7 +107,7 @@ export class New extends React.Component {
           slidesToSlide={1}
           swipeable
         >
-          {this.processItemsToChildren(Items)}
+          {this.processItemsToChildren(Items, cart)}
         </Carousel>
       )
     }
