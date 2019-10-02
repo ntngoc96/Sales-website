367f0b8e45ec42c5c0c9c8cb118004a304d593b4
Nguyen Thai Ngoc <ntngoc96>
2019-10-02 11:50:20 +0700

    Updates

---
+++ precache-manifest.df90c13fc6b7058bbb061ea3ca91f00a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e20c1ef4e1f929a6475bb928376f5f48",
+    "url": "/Sales-website/index.html"
+  },
+  {
+    "revision": "e65197c6e231284fc23c",
+    "url": "/Sales-website/static/css/2.22dd386b.chunk.css"
+  },
+  {
+    "revision": "7ddc683d9feb25514d06",
+    "url": "/Sales-website/static/css/main.cf676543.chunk.css"
+  },
+  {
+    "revision": "e65197c6e231284fc23c",
+    "url": "/Sales-website/static/js/2.645a028b.chunk.js"
+  },
+  {
+    "revision": "7ddc683d9feb25514d06",
+    "url": "/Sales-website/static/js/main.43900e89.chunk.js"
+  },
+  {
+    "revision": "22c874197bc7b514ea69",
+    "url": "/Sales-website/static/js/runtime~main.8a74aad6.js"
+  },
+  {
+    "revision": "e6ec1c01f556d1016336ea63f4a616e5",
+    "url": "/Sales-website/static/media/10858.e6ec1c01.jpg"
+  },
+  {
+    "revision": "228b88233937aac67b154eff25da89ed",
+    "url": "/Sales-website/static/media/cat-logo.228b8823.jpg"
+  },
+  {
+    "revision": "0d1ca39b9410f5fe2deb3a5a12cd947f",
+    "url": "/Sales-website/static/media/certificate.0d1ca39b.jpg"
+  },
+  {
+    "revision": "289f563ed3907cb30725c541026c2111",
+    "url": "/Sales-website/static/media/featured_1.289f563e.jpg"
+  },
+  {
+    "revision": "5a4e0d909bd08e3310b4a764a79db5f2",
+    "url": "/Sales-website/static/media/featured_2.5a4e0d90.jpg"
+  },
+  {
+    "revision": "dbb1c35e2c324b4db16933b6b51a6abd",
+    "url": "/Sales-website/static/media/featured_3.dbb1c35e.jpg"
+  },
+  {
+    "revision": "a7a5329eda89e581a3dc2382ce4dfd7d",
+    "url": "/Sales-website/static/media/featured_4.a7a5329e.jpg"
+  },
+  {
+    "revision": "ad7c84efa28e9ab4adb4de296727ce3f",
+    "url": "/Sales-website/static/media/featured_5.ad7c84ef.jpg"
+  },
+  {
+    "revision": "bd3bdc73c8f1e6a01e5c3e501c1590b9",
+    "url": "/Sales-website/static/media/featuredmain_1.bd3bdc73.jpg"
+  },
+  {
+    "revision": "aad14d92a4789a7eecdd644c555fee45",
+    "url": "/Sales-website/static/media/featuredmain_2.aad14d92.jpg"
+  },
+  {
+    "revision": "36a5bdf89eed7580173f9f131c33bb19",
+    "url": "/Sales-website/static/media/logo_miumiu_spa_main.36a5bdf8.png"
+  },
+  {
+    "revision": "913c9efd4d4e88186d8f157c9490cdc9",
+    "url": "/Sales-website/static/media/logo_transparent.913c9efd.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/Sales-website/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/Sales-website/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/Sales-website/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "8854304af5fa56e2cbf86b39d002d59d",
+    "url": "/Sales-website/static/media/story_1.8854304a.jpg"
+  },
+  {
+    "revision": "a345e40a94d8706451a0fe585e5e0d8d",
+    "url": "/Sales-website/static/media/video2.a345e40a.mov"
+  }
+]);
